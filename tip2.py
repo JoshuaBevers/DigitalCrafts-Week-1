8bbb6a68816b19a5c83f906f5ad55d1119985065
infernezor@gmail.com <infernezor@gmail.com>
2020-01-15 20:37:06 -0500

    Updated comment for exercise.

--- tip2.py
+++ tip2.py
@@ -7,5 +7,5 @@ print("the total tip is: " + str(total))
 split = float(input("How many ways are you splitting the bill?"))
 print("The split totals are: " + str(total / split))
 
-
+# This was to conform closer to what the actual exercise was for the class. Not particularily happy with this one.
 # service2 = .1 if tip == "bad" else .2 if tip == "good" and tip != "fair"  else .15
